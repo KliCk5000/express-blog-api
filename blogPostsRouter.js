e01ec96c2bdad901809ee467eab68bf418bcbb72
Nick Dean <klick5000@gmail.com>
2018-12-03 16:51:11 -0700

    add GET, POST, DELETE, and PUT routes

---
+++ blogPostsRouter.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+
+const { BlogPosts } = require("./models");
+
+// Create some blog posts to look at
+BlogPosts.create("Blog 1", "This is the content of blog 1.", "Nick Dean");
+BlogPosts.create("Blog 2", "This is the content of blog 2.", "Nick Dean");
+BlogPosts.create("Blog 3", "This is the content of blog 3.", "Nick Dean");
+
+// send back JSON representation of all blog posts
+// on GET requests to root
+router.get("/", (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+router.post("/", jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ["title", "content", "author"];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(
+    req.body.title,
+    req.body.content,
+    req.body.author
+  );
+  res.status(201).json(item);
+});
+
+router.delete("/:id", (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post item \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+router.put("/:id", jsonParser, (req, res) => {
+  const requiredFields = ["title", "content", "author"];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = `Request path id (${
+      req.params.id
+    }) and request body id ``(${req.body.id}) must match`;
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post item \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  });
+  res.status(204).end();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const morgan = require('morgan');
 
 const app = express();
 
+const BlogPosts = require('./blogPostsRouter');
+
 app.use(morgan('common'));
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use('/blog-posts', BlogPosts);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
